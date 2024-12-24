a4b87895d4fba365b033afa8149ce3688da25dee
Brendan Bank <brendan@BGW-BB003.bgwlan.nl>
2024-12-24 23:20:34 +0100

    more stuff

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,5 @@
 import sys, os, ssl
+import asyncio
 sys.path.append('/Users/brendan/src/fancontrol/lib')
 
 try:
@@ -11,20 +12,71 @@ try:
 except:
     pass
 
-from microdot import Microdot, Response, send_file, abort
-from microdot.utemplate import Template
-from utemplate import recompile
-import ulogging as logging
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+from microdot import Microdot, Response, send_file, abort
+from microdot.utemplate import Template
+from microdot.session import Session, with_session
+from microdot_login import authorization_required
+
+from utemplate import recompile
+
+from config import config
+
+
 app = Microdot()
+Session(app, secret_key='top-secret')
 Response.default_content_type = 'text/html'
+
 
 Template.initialize(loader_class=recompile.Loader)
     
 application_vars = {}
 application_vars["application_name"] = "Fan Control"
+
+
+@app.route('/')
+@with_session
+@authorization_required
+async def index(req, session):
+    return Template('index.html').render(page='Home',application=application_vars)
+
+@app.route('/test')
+async def test(request):
+    
+    fields = [
+            {
+                'name': 'ssid',
+                'text': 'SSID',
+                'type': "text",
+                'columns': "col-sm-6",
+                'error': False,
+                'error_txt': 'Valid SSID is required.',
+                'value': ""
+            },
+            {
+                'name': "password",
+                'text': "Password",
+                'type': "password",
+                'columns': "col-sm-6",
+                'error': True,
+                'error_txt': 'Valid password is required.',
+                'value': ""
+            },
+            {
+                'name': "hostname",
+                'text': "Hostname",
+                'type': "text",
+                'columns': "col-12",
+                'error': False,
+                'error_txt': 'Please enter a Device Name (no spaces or special characters allowed).',
+                'value': "hostname"
+            },
+        ]
+
+    return Template('test.html').render(page='Test',application=application_vars, form_fields=fields)
 
 
 @app.route('/static/<path:path>')
@@ -36,21 +88,30 @@ async def static(request, path):
         abort(404, reason="File not found")
     return send_file('static/' + path, compressed=True, file_extension='.gz', max_age=(3600))
 
-@app.route('/')
-async def index(req):
-    return Template('index.html').render(page='Home',application=application_vars)
 
 @app.route('/settings/<string:setting_name>', methods=['GET', 'POST'])
 async def setting_name(req, setting_name):
     print (f'req form = {req.form}')
     return Template('wifi.html').render(page='Network Configuration Settings', application=application_vars)
 
-
-if __name__ == '__main__':
+async def task():
     ext="der"
     logging.basicConfig(level=logging.INFO)
     sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    sslctx.verify_mode = ssl.CERT_NONE
     sslctx.load_cert_chain(certfile='cert/cert.der', keyfile = 'cert/key.' + ext)
-    
-    app.run(port=5999, debug=True)
+    await app.start_server(host="0.0.0.0", port=5999, debug=True)
+#     await app.start_server(host="0.0.0.0", port=5999, debug=True, ssl=sslctx)
+
+if __name__ == '__main__':
+    asyncio.run(task())
+#     ext="der"
+#     logging.basicConfig(level=logging.INFO)
+#     sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+#     sslctx.verify_mode = ssl.CERT_NONE
+#     sslctx.load_cert_chain(certfile='cert/cert.der', keyfile = 'cert/key.' + ext)
+#     
+# #     app.run(port=5999, debug=True)
+# 
+# 
 #     app.run(port=5999, debug=True, ssl=sslctx)
--- f_test.py
+++ f_test.py
@@ -1,10 +1,9 @@
 import sys
 sys.path.append('/Users/brendan/src/fancontrol/lib')
 import os
-import ulogging as logging
+import logging
 
 logging.basicConfig(level=logging.DEBUG)
-
 log = logging.getLogger(__name__)
 
 
@@ -13,14 +12,15 @@ from configuration import Configuration
 if __name__ == "__main__":
     
     log.debug("started")
-    dict_obj = {}
-    config = Configuration(dict_obj, configfile="/Users/brendan/src/fancontrol/config.json")
+    DICT_OBJ = dict ()
+    config = Configuration(DICT_OBJ, configfile="/Users/brendan/src/fancontrol/config.json")
 #     
 # #     config.namespace("myhope")
 # #     config.myhope.now = 1
 #     
-    wifi = config.get_config_item('wifi')
-    
-    config_vars = wifi.configuration
-    print (config_vars.ssid)
-    print (config.wifi.ssid)
+#     wifi = config.get_config_item('wifi')
+#     
+#     config_vars = wifi.configuration
+#     print (config_vars.ssid)
+#     print (config.wifi.ssid)
+# 
